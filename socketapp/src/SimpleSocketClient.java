b39886bad62735ae54748547ee9b8e1f0d26d7d1
ShannonYLu <53765441+ShannonYLu@users.noreply.github.com>
2022-10-03 00:12:22 +1300

    Trying to read server message but not working

--- socketapp/src/SimpleSocketClient.java
+++ socketapp/src/SimpleSocketClient.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.net.InetAddress;
@@ -12,56 +15,52 @@ public class SimpleSocketClient
 {
     public static void main( String[] args ) throws UnknownHostException
     {
-//    	    # get the hostname
-//    	    host = socket.gethostname()
-//    	    port = 5000  # initiate port no above 1024
+
         String server = InetAddress.getLocalHost().getHostName();
         int port = 8080;
 
         System.out.println( "Loading contents of URL: " + server );
 
         try {
-//    	    ClientMultiSocket = Socket.socket();
-//    	    ClientMultiSocket.settimeout(100)
+
             // Connect to the server
             Socket socket = new Socket( server, port );
 
+            // Writer to send message to server
+            Writer out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
+           
+            
+           while(true) {
+        	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   	       String dataStr;
+	   	       System.out.println("Hey there: ");
+	   	       dataStr = br.readLine();
+	   	       
+	   	       if (dataStr == "Bye"){
+	  	         break;
+	   	       }
+        	        	
+	   	       out.append(dataStr).append("\n");
+	   	       out.flush();
+	   	       
+            boolean acknowledged = false;
+            
+			while (!acknowledged) {
+	            // StreamReader to read the response from the server
+	            InputStreamReader streamReader = new InputStreamReader(socket.getInputStream(), "UTF-8");
+	            // BufferedReader reader = new BufferedReader(streamReader);
+	            
+//				String responseMsg;
+				StringBuilder sb = new StringBuilder();
 
-            System.out.println("Waiting for connection response");
-//    	    try:
-//    	        ClientMultiSocket.connect((host, port))
-//    	    except socket.error as e:
-//    	        print(str(e))
-//    	    res = ClientMultiSocket.recv(1024)
-//
-    	    while (true) {
-    	    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	    	String dataStr;
-    	    	System.out.println("Hey there: ");
-    	        dataStr = br.readLine();
+				for (int ch; (ch = streamReader.read()) != -1; ) {
+				    sb.append((char) ch);
+				}
+				System.out.println(sb.toString());
+	        	acknowledged = true;
 
-    	        if (dataStr == "Bye"){
-    	          break;
-    	        }
-    	        int dataInt = Integer.parseInt(dataStr);
-    	        
-    	        OutputStream output = socket.getOutputStream(); // output to the server
-    	        output.write(dataInt);
-    	        PrintWriter writer = new PrintWriter(output, true);
-    	        
-//    	        ClientMultiSocket.send(str.encode(Input))
-//    	        acknowledged = False
-//    	        while not acknowledged:
-//    	            try {
-                ObjectInputStream res = new ObjectInputStream(socket.getInputStream());
-                String message = (String) res.readObject();
-                System.out.println("From (" + (String)(server) + "," + port + "): " + res);
-//    	                acknowledged = True
-//    	            }
-//    	            except socket.timeout{
-//    	    }
     	    }
-//    	    ClientMultiSocket.close()
+           }
             socket.close();
         }
         catch( Exception e )
